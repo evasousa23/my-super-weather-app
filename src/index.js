4284773e539d4e7ba1574ac15f735fa0ec586283
evasousa23 <sousa.eva99@gmail.com>
2023-11-30 18:48:19 +0000

    remove the img from the div(weather-forecast)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 function updateWeatherData(response) {
+  let iconElement = document.querySelector("#icon");
   let temperatureElement = document.querySelector("#current-weather");
   let temperature = response.data.temperature.current;
   let descriptionElement = document.querySelector("#description");
@@ -8,15 +9,13 @@ function updateWeatherData(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-  let iconElement = document.querySelector("#icon");
 
 
-
-iconElement.innerHTML = `<img src="${ response.data.condition.icon_url}" >`; 
-  cityElement.innerHTML = response.data.city;
-  countryElement.innerHTML = response.data.country;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" >`;
   temperatureElement.innerHTML = Math.round(temperature);
   descriptionElement.innerHTML = response.data.condition.description;
+  cityElement.innerHTML = response.data.city;
+  countryElement.innerHTML = response.data.country;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = response.data.wind.speed;
   timeElement.innerHTML = formatDate(date);
@@ -43,7 +42,7 @@ function formatDate(date) {
   }
   return `${day} , ${hours}:${minutes}`;
 }
-console.log(formatDate);
+
 function searchCity(city) {
   let apiKey = "a3af3t89ob96e0f9a620e92909b7a4bf";
   let apiUrl = ` https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
