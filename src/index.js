b19f429504c86ff33340c774956b98cfe9199df3
evasousa23 <sousa.eva99@gmail.com>
2023-11-30 11:47:20 +0000

    create a response function
    create an id (current-weather) and create the function updateWeatherData, to call the id

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+function updateWeatherData(response) {
+  let temperatureElement = document.querySelector("#current-weather");
+
+  temperatureElement.innerHTML = response.data.temperature.current;
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "a3af3t89ob96e0f9a620e92909b7a4bf";
+  let apiUrl = ` https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function searchFormInput(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchFormInput);
-
