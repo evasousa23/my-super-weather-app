5178c8e4df1ee649bbd69dc5ebde666635cf673f
evasousa23 <sousa.eva99@gmail.com>
2023-11-30 16:54:59 +0000

    created a function to display the date, time and minutes in real time

--- src/index.js
+++ src/index.js
@@ -3,15 +3,43 @@ function updateWeatherData(response) {
   let temperature = response.data.temperature.current;
   let descriptionElement = document.querySelector("#description");
   let cityElement = document.querySelector("#city");
+  let countryElement = document.querySelector("#country");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
   cityElement.innerHTML = response.data.city;
+  countryElement.innerHTML = response.data.country;
   temperatureElement.innerHTML = Math.round(temperature);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
-
-  console.log(response.data);
+  windElement.innerHTML = response.data.wind.speed;
+  timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  return `${day} , ${hours}:${minutes}`
+}
+console.log(formatDate);
 function searchCity(city) {
   let apiKey = "a3af3t89ob96e0f9a620e92909b7a4bf";
   let apiUrl = ` https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
