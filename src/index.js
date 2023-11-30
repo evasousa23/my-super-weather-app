f24141583550ee6e7d374e455d9804a61232e023
evasousa23 <sousa.eva99@gmail.com>
2023-11-30 15:29:44 +0000

    add more java script

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 function updateWeatherData(response) {
   let temperatureElement = document.querySelector("#current-weather");
+  let temperature = response.data.temperature.current;
+  let descriptionElement = document.querySelector("#description");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
 
-  temperatureElement.innerHTML = response.data.temperature.current;
-  console.log(response.data.temperature.current);
+  console.log(response.data);
 }
 
 function searchCity(city) {
   let apiKey = "a3af3t89ob96e0f9a620e92909b7a4bf";
   let apiUrl = ` https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(updateWeatherData);
 }
 
 function searchFormInput(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
